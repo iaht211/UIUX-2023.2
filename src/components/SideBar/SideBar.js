db447adf923f54927597951752667294538ab34d
iaht211 <123443192+iaht211@users.noreply.github.com>
2024-05-25 20:50:25 +0700

    fix color navbar home page

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -9,11 +9,10 @@ import {
     SidebarContent,
 } from 'react-pro-sidebar';
 
-import { FaTachometerAlt, FaGem, FaList, FaGithub, FaRegLaughWink, FaHeart } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 import { GiReactor } from "react-icons/gi";
-import sidebarBg from '../../assets/bg2.jpg';
 import './SideBar.scss'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FaHome } from "react-icons/fa";
 import { SlCalender } from "react-icons/sl";
 import { FaBoxArchive } from "react-icons/fa6";
@@ -22,102 +21,99 @@ import { IoMdSettings } from "react-icons/io";
 
 
 
+
 const SideBar = (props) => {
-    const { image, collapsed, toggled, handleToggleSidebar } = props;
+    const { collapsed, toggled, handleToggleSidebar } = props;
+    const location = useLocation();
+    const { pathname } = location;
+
     return (
-        <>
-            <ProSidebar
-                image={sidebarBg}
-                collapsed={collapsed}
-                toggled={toggled}
-                breakPoint="md"
-                onToggle={handleToggleSidebar}
-            >
-                <SidebarHeader>
-                    <div
-                        style={{
-                            padding: '24px',
-                            textTransform: 'uppercase',
-                            fontWeight: 'bold',
-                            fontSize: 14,
-                            letterSpacing: '1px',
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                            whiteSpace: 'nowrap',
-                        }}
+        <ProSidebar
+            collapsed={collapsed}
+            toggled={toggled}
+            breakPoint="md"
+            onToggle={handleToggleSidebar}
+        >
+            <SidebarHeader>
+                <div
+                    style={{
+                        padding: '24px',
+                        textTransform: 'uppercase',
+                        fontWeight: 'bold',
+                        fontSize: 14,
+                        letterSpacing: '1px',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        whiteSpace: 'nowrap',
+                    }}
+                >
+                    <GiReactor size={'3em'} color='00bfff' />
+                    <span>KPI</span>
+                </div>
+            </SidebarHeader>
+
+            <SidebarContent>
+                <Menu iconShape="circle">
+                    <MenuItem
+                        icon={<FaHome />}
+                        className={pathname === '/' ? 'active' : ''}
                     >
-                        <GiReactor size={'3em'} color='00bfff' />
-                        <span>Hoi Dan IT</span>
-                    </div>
-                </SidebarHeader>
-
-                <SidebarContent>
-                    <Menu iconShape="circle">
-                        <MenuItem
-                            icon={<FaHome />}
-                        >
-                            Trang chủ
-                            <Link to="/" />
-                        </MenuItem>
-                    </Menu>
-                    <Menu iconShape="circle">
-                        <MenuItem
-                            icon={<FaBoxArchive />}
-                        >
-                            KPI
-                            <Link to="/kpi" />
-                        </MenuItem>
-
-                    </Menu>
-                    <Menu iconShape="circle">
-                        <MenuItem
-                            icon={<FaChartBar />}
-                        >
-                            Thống kê
-                            <Link to="/statistical" />
-                        </MenuItem>
-                    </Menu>
-                    <Menu iconShape="circle">
-                        <MenuItem
-                            icon={<SlCalender />}
-                        >
-                            Lịch
-                            <Link to="/calender" />
-                        </MenuItem>
-                    </Menu>
-                    <Menu iconShape="circle">
-                        <MenuItem
-                            icon={<IoMdSettings />}
-                        >
-                            Cài đặt
-                            <Link to="/setting" />
-                        </MenuItem>
-                    </Menu>
-                </SidebarContent>
-
-                <SidebarFooter style={{ textAlign: 'center' }}>
-                    <div
-                        className="sidebar-btn-wrapper"
-                        style={{
-                            padding: '20px 24px',
-                        }}
+                        Trang chủ
+                        <Link to="/" />
+                    </MenuItem>
+                    <MenuItem
+                        icon={<FaBoxArchive />}
+                        className={pathname === '/kpi' ? 'active' : ''}
                     >
-                        <a
-                            href="https://github.com/azouaoui-med/react-pro-sidebar/tree/v0.x"
-                            target="_blank"
-                            className="sidebar-btn"
-                            rel="noopener noreferrer"
-                        >
-                            <FaGithub />
-                            <span style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}>
-                                react pro side bar old version
-                            </span>
-                        </a>
-                    </div>
-                </SidebarFooter>
-            </ProSidebar>
-        </>
-    )
-}
+                        KPI
+                        <Link to="/kpi" />
+                    </MenuItem>
+                    <MenuItem
+                        icon={<FaChartBar />}
+                        className={pathname === '/statistical' ? 'active' : ''}
+                    >
+                        Thống kê
+                        <Link to="/statistical" />
+                    </MenuItem>
+                    <MenuItem
+                        icon={<SlCalender />}
+                        className={pathname === '/calender' ? 'active' : ''}
+                    >
+                        Lịch
+                        <Link to="/calender" />
+                    </MenuItem>
+                    <MenuItem
+                        icon={<IoMdSettings />}
+                        className={pathname === '/setting' ? 'active' : ''}
+                    >
+                        Cài đặt
+                        <Link to="/setting" />
+                    </MenuItem>
+                </Menu>
+            </SidebarContent>
+
+            <SidebarFooter style={{ textAlign: 'center' }}>
+                <div
+                    className="sidebar-btn-wrapper"
+                    style={{
+                        padding: '20px 24px',
+                    }}
+                >
+                    <a
+                        href="https://github.com/azouaoui-med/react-pro-sidebar/tree/v0.x"
+                        target="_blank"
+                        className="sidebar-btn"
+                        rel="noopener noreferrer"
+                    >
+                        <FaGithub />
+                        <span style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}>
+                            react pro side bar old version
+                        </span>
+                    </a>
+                </div>
+            </SidebarFooter>
+        </ProSidebar>
+    );
+};
 
 export default SideBar;
