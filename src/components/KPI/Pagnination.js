393ebe40e895721c9949075789b83b39441fd682
iaht211 <123443192+iaht211@users.noreply.github.com>
2024-06-06 20:58:28 +0700

    fix kpi web

--- src/components/KPI/Pagnination.js
+++ src/components/KPI/Pagnination.js
@@ -2,7 +2,7 @@ const Pagnination = () => {
     return (
         <>
             <nav aria-label="Page navigation example">
-                <ul class="pagination">
+                <ul class="pagination" style={{marginLeft: '430px'}}>
                     <li class="page-item"><a class="page-link" href="#">Previous</a></li>
                     <li class="page-item"><a class="page-link" href="#">1</a></li>
                     <li class="page-item"><a class="page-link" href="#">2</a></li>
